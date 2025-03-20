db6ae9a7ed021936bc2decab500c4a0bcb04a94d
kedziorno <kjoblak85@gmail.com>
2025-03-20 10:25:40 +0100

    pytest_c_testrunner

--- run_testrunner.sh
+++ run_testrunner.sh
@@ -4,6 +4,8 @@ set -x
 TESTRUNNER="pytest_c_testrunner"
 TESTRUNNER_PATH="${TESTRUNNER}.patch"
 
+git submodule add -b master --name pytest_c_testrunner https://github.com/jmcnamara/pytest_c_testrunner.git pytest_c_testrunner
+
 rm -rf testrunner
 mkdir testrunner
 cd testrunner
